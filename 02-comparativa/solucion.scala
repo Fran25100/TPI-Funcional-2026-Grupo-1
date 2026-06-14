4e8b2bc4248a157138b0e4d0e90405ac2974c1fd
Fran25100 <kuritokurokurote@gmail.com>
2026-06-14 15:02:34 -0300

    Update solucion.scala

--- 02-comparativa/solucion.scala
+++ 02-comparativa/solucion.scala
@@ -27,6 +27,82 @@ List(en-amarillo, cambiar-a-rojo)
 */
 
 /*funcion timer*/
-def timer() = {
-  
+def timer(timestap: Float): String = {//funcion timer que recibe el parametro timestap
+  (timestap % 215.0f) match {//declaro el match con el parametro timestap
+    // Aplicamos el módulo 216.0f para limpiar los ciclos completos, siempre va a valer entre 0 y 215(como si fuera de 1-216).
+    case time if time >= 0.0f && time <= 86f.0 => "en-rojo"
+    // caso 1. => comparamos si esta dentro del rango para ser rojo
+    case time if time >= 86.0f && time <= 89.0f => "en-rojo-intermitente"
+    // caso 2. => comparamos si esta dentro del rango para ser rojo-intermitente
+    case time if time >= 89.0f && time <= 206.0f => "en-verde"
+    // caso 3. => comparamos si esta dentro del rango para ser verde
+    case time if time >= 207.0f && time <= 209.0f => "en-verde-intermitente"
+    // caso 4. => comparamos si esta dentro del rango para ser verde-intermitente
+    case time if time >= 210.0f && time <= 212.0f => "en-amarillo"
+    // caso 5. => comparamos si esta dentro del rango para ser amarillo
+    case time if time >= 213.0f && time <= 215.0f => "en-amarillo-intermitente"
+    // caso 6. => comparamos si esta dentro del rango para ser amarillo-intermitente
+  }
 }
+/*
+//casos probados
+timer(1000)
+en-verde
+
+timer(0)
+en-rojo
+
+timer(27)
+en-rojo
+
+timer(86)
+en-rojo
+
+timer(87)
+en-rojo-intermitente
+
+timer(88)
+en-rojo-intermitente
+
+timer(89)
+en-rojo-intermitente
+
+timer(90)
+en-verde
+
+timer(170)
+en-verde
+
+timer(206)
+en-verde
+
+timer(207)
+en-verde-intermitente
+
+timer(208)
+en-verde-intermitente
+
+timer(209)
+en-verde-intermitente
+
+timer(210)
+en-amarillo
+
+timer(211)
+en-amarillo
+
+timer(212)
+en-amarillo
+
+timer(213)
+en-amarillo-intermitente
+
+timer(214)
+en-amarillo-intermitente
+
+timer(215)
+en-amarillo-intermitente
+
+timer(2000)
+en-rojo
+*/
