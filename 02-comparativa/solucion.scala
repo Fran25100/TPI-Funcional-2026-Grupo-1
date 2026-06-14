787c13cbd0d3ff7cebafef7fea9fe9f8a404d76a
Fran25100 <kuritokurokurote@gmail.com>
2026-06-13 22:43:48 -0300

    Update solucion.scala

--- 02-comparativa/solucion.scala
+++ 02-comparativa/solucion.scala
@@ -1,16 +1,23 @@
 /* funcion: transicion */
 def transicion(colorActual: String, colorACambiar: String) = { // declare la funcion, como parametro las variables strings
   (colorActual, colorACambiar) match { // funciona como un cond, donde los 2 parametros se comparan en cada caso
-    case ("rojo", "verde") => (println(s"$colorActual rojoIntermitente cambia a $colorACambiar"))
+    case ("rojo", "verde") => (List (colorActual ,colorActual,"Intermitente cambia-a-",colorACambiar))
     // caso1. => indica que hacia al lado donde apunta es el resultado, en este caso el mensaje a imprimir
-    case ("verde", "amarillo") => (println(s"$colorActual verdeIntermitente cambia a $colorACambiar"))
+    case ("verde", "amarillo") => (List (colorActual ,colorActual,"Intermitente cambia-a-",colorACambiar))
     // caso 2. => indica que hacia al lado donde apunta es el resultado, en este caso el mensaje a imprimir
-    case ("amarillo", "rojo") => (println( s"$colorActual amarilloIntermitente cambia a $colorACambiar"))
+    case ("amarillo", "rojo") => (List (colorActual ,colorActual,"Intermitente cambia-a-",colorACambiar))
     // caso 3. => indica que hacia al lado donde apunta es el resultado, en este caso el mensaje a imprimir
+    case _ => (List(colorActual, "accion-por-defeto"))
+    // caso 4. => si no es como los otros 3 casos devuelve una lista con el colorActual y el string "accion-por-defecto"
   }
 }
-//no esta terminado todavia no devuelve de manera exacta(no devuelve una lista)
 
-//caso probado
-//rojo rojoIntermitente cambia a verde
+
+/*casos probados
+transicion("rojo", "amarillo")
+List(rojo, accion-por-defeto)
+
 transicion("rojo", "verde")
+List(rojo, rojo,intermitente color-a-,verde )
+
+*/
