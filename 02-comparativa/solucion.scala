7d65c5cfd8167240890f2238332e38b42e86fddb
Axel100lp <axel.a.lezcano@gmail.com>
2026-06-13 22:50:01 -0300

    actualizacion

--- 02-comparativa/solucion.scala
+++ 02-comparativa/solucion.scala
@@ -1,16 +1,21 @@
 /* funcion: transicion */
-def transicion(colorActual: String, colorACambiar: String) = { // declare la funcion, como parametro las variables strings
+def transicion(colorActual: String, colorACambiar: String): List[String] = { // declare la funcion, como parametro las variables strings
   (colorActual, colorACambiar) match { // funciona como un cond, donde los 2 parametros se comparan en cada caso
-    case ("rojo", "verde") => (List (colorActual ,colorActual,"Intermitente cambia-a-",colorACambiar))
-    // caso1. => indica que hacia al lado donde apunta es el resultado, en este caso el mensaje a imprimir
-    case ("verde", "amarillo") => (List (colorActual ,colorActual,"Intermitente cambia-a-",colorACambiar))
-    // caso 2. => indica que hacia al lado donde apunta es el resultado, en este caso el mensaje a imprimir
-    case ("amarillo", "rojo") => (List (colorActual ,colorActual,"Intermitente cambia-a-",colorACambiar))
-    // caso 3. => indica que hacia al lado donde apunta es el resultado, en este caso el mensaje a imprimir
-    case _ => (List(colorActual, "accion-por-defeto"))
-    // caso 4. => si no es como los otros 3 casos devuelve una lista con el colorActual y el string "accion-por-defecto"
+    case ("en-rojo", "verde") => List(colorActual, "cambiar-a-verde")
+    // caso1. => indica hacia que lado apunta el resultado, en este caso el mensaje a imprimir
+    case ("en-verde", "amarillo") => List(colorActual, "cambiar-a-amarillo")
+    // caso 2. => indica hacia que lado apunta el resultado, en este caso el mensaje a imprimir
+    case ("en-amarillo", "rojo") => List(colorActual, "cambiar-a-rojo")
+    // caso 3. => indica hacia que lado apunta el resultado, en este caso el mensaje a imprimir
+    case _ => List(colorActual, "accion-por-defecto")
   }
 }
+//no esta terminado todavia no devuelve de manera exacta(no devuelve una lista)
+
+//caso probado
+//rojo rojoIntermitente cambia a verde
+transicion("rojo", "verde")
+
 
 
 /*casos probados
