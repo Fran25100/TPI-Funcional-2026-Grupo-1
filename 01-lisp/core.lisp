777cbef1369e1d9a37039be3bae09b2427bb8c04
Axel100lp <axel.a.lezcano@gmail.com>
2026-06-15 15:00:26 -0300

    Update core.lisp

--- 01-lisp/core.lisp
+++ 01-lisp/core.lisp
@@ -283,7 +283,7 @@ IMPACTO: no destructiva
    (format stream "=========================================~%")
    (logginLights color-actual cambio-color)
 (format stream "~A: transicion: ~A --> ~A"
-(local-time:format-timestring nil (local-time:now):format '((:year 4) "-" (:month 2) "-" (:day 2) " " (:hour 2) ":" (:min 2)))
+(local-time:format-timestring stream (local-time:now) :format '((:year 4) "-" (:month 2) "-" (:day 2) " " (:hour 2) ":" (:min 2)))
 (car (transicion color-actual cambio-color)) (caddr (transicion color-actual cambio-color)) )
 (format stream "~% --- Fin del Informe ---") ) )
 ;pasandole loggin para ver en pantalla y luego la impresion dentro del archvio
